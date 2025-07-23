888f13f20d8f76c0a2bf13c55abfd86814f7e713
vanrobo <vanrobogaming@gmail.com>
2025-07-23 10:01:19 +0530

    afaefaef

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-// postcss.config.js
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {}, // This is the updated line
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
-};
+}
